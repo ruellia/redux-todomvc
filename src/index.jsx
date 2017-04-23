6232953279c0733076f59b512e14a07e2cf8d040
Ruellia Zhang <syncyes@gmail.com>
2017-04-22 20:37:41 -0700

    wire up to redux with simple state setting reducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {List, Map} from 'immutable';
+import {compose, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import {TodoAppContainer} from './components/TodoApp';
 
-import TodoApp from './components/TodoApp';
+const createStoreDevTools = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
 
-const todos = List.of(
-  Map({id: 1, text: 'React', status: 'active', editing: false}),
-  Map({id: 2, text: 'Redux', status: 'active', editing: false}),
-  Map({id: 3, text: 'Immutable', status: 'completed', editing: false})
-);
+const store = createStoreDevTools(reducer);
 
-const filter = 'all';
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+      todos: [
+      {id: 1, text: 'React', status: 'active', editing: false},
+      {id: 2, text: 'Redux', status: 'active', editing: false},
+      {id: 3, text: 'Immutable', status: 'active', editing: false},
+    ],
+    filter: 'all'
+  }
+})
 
 require('../node_modules/todomvc-app-css/index.css');
 
 ReactDOM.render(
-  <TodoApp todos={todos} filter = {filter}/>,
+  <Provider store={store}>
+    <TodoAppContainer />
+  </Provider>,
   document.getElementById('app')
 );
