307c36a4bbbcc4ef9fd850c79c40819923b02e41
Ruellia Zhang <syncyes@gmail.com>
2017-04-22 19:19:20 -0700

    add todoheader and unit tests

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from './TodoList'
+import TodoHeader from './TodoHeader'
 
 export default class TodoApp extends React.Component {
   render() {
     return <div>
       <section className="todoapp">
+        <TodoHeader />
         <TodoList todos={this.props.todos} filter={this.props.filter}/>
       </section>
     </div>
---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default class TodoHeader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.shouldComponentUpdate = PureRenderMixin
+            .shouldComponentUpdate
+            .bind(this);
+    }
+
+    _handleKeyPress(e) {
+        if (e.key === 'Enter' && this.refs.addTodoInput.value !== '') {
+            return this
+                .props
+                .addItem(this.refs.addTodoInput.value);
+        }
+    }
+
+    render() {
+        return <header className="header">
+            <h1>todos</h1>
+            <input
+                className="new-todo"
+                ref="addTodoInput"
+                autofocus
+                autoComplete="off"
+                placeholder="What needs to be done?"
+                onKeyPress={this._handleKeyPress.bind(this)}/>
+        </header>
+    }
+};
